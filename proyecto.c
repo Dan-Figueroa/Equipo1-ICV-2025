fdeb3b3e9f475557d03223df760cc80fb7a4de81
Nimynus <nimynus@gmail.com>
2025-06-09 14:31:46 -0600

    menu principal distancias

--- proyecto.c
+++ proyecto.c
@@ -1,1 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(){
+    int op;
+    menuprincipal(op);
+}
+
+int menuprincipal(int op){
+    printf("Bienvenido al conversor de unidades\n");
+    printf("1 Temperatura\n");
+    printf("2 Distancia");
+
+    return op;
+}
+
+int distacias(int op){
+
+}
 
