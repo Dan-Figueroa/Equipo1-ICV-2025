729945d009a8c5da562348a075cb32a66caf12c4
memocontr <contr.memo@gmail.com>
2025-06-09 14:42:15 -0600

    menu principal

--- proyecto.c
+++ proyecto.c
@@ -1,1 +1,14 @@
+#include<stdio.h>
+#include<string.h>
 
+int main(){
+	int op;
+
+}
+
+int menuprincipal(op){
+	printf("Bienvenido al menu de converisones\n");
+	printf("1. Temperaturas\n");
+	printf("2. Distancias\n");
+	return	op;
+}
